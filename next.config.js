164e2a2c7e133caef5e119005be1203eef553fec
Nathan <nathan@stytch.com>
2021-08-04 11:26:14 -0700

    Add env vars

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     STYTCH_PUBLIC_TOKEN: process.env.STYTCH_PUBLIC_TOKEN,
+    IRON_SESSION_PWD: process.env.IRON_SESSION_PWD,
+    COOKIE_NAME: process.env.COOKIE_NAME,
   },
 };
