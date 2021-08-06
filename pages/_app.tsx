b8aa56c6e954186554d681b6ae27858887e0c68f
allison-stytch <85202371+allison-stytch@users.noreply.github.com>
2021-08-06 14:39:56 -0700

    Update svg and app title (#2)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <title>Stytch + Next.js example app</title>
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 export default MyApp
