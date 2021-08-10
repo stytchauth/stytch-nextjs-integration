042b12974da8619dc5adbd786fae294f27d2a629
allison-stytch <85202371+allison-stytch@users.noreply.github.com>
2021-08-10 10:10:03 -0700

    Add navbar (#6)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import styles from "../styles/Home.module.css";
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import React from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
+import logo from '/public/stytch-logo.svg';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +14,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <title>Stytch + Next.js example app</title>
       </Head>
+      <div className={styles.nav}>
+        <a href="https://stytch.com" rel="noopener noreferrer" target="_blank">
+          <Image alt="Stytch logo" height={20} src={logo} width={105} />
+        </a>
+        <span className={styles.accountNavItem}>Account</span>
+      </div>
       <div className={styles.root}>
         <Component {...pageProps} />
       </div>
