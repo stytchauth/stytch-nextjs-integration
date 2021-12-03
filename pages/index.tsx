790cfcb34324760d0c17c4457bc64b6ec82d8954
Chris <91094210+chris-stytch@users.noreply.github.com>
2021-12-03 12:11:44 -0800

    Use optional chaining and update comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,12 @@ import LoginEntryPoint from '../components/LoginEntryPoint';
 // 1. Running locally via `vercel dev`; VERCEL_URL will contain localhost, but will not be https.
 // 2. Deploying via Vercel; VERCEL_URL will be generated on runtime and use https.
 // 3. Running locally via `npm run dev`; VERCEL_URL will be undefined and the app will be at localhost.
+//
+// VERCEL_URL only contains the domain of the site's URL, the scheme is not included so we must add it manually,
+// see https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables.
 let REDIRECT_URL_BASE = '';
 
-if (process.env.VERCEL_URL != undefined &&
-    process.env.VERCEL_URL.includes('localhost')) {
+if (process.env.VERCEL_URL?.includes('localhost')) {
   REDIRECT_URL_BASE = 'http://localhost:3000';
 } else if (process.env.VERCEL_URL != undefined) {
   REDIRECT_URL_BASE = `https://${process.env.VERCEL_URL}`;
