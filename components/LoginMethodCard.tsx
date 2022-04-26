eab61cb570d396ae01f8357a01904e6474d2d6a3
Cal Rueb <cal@stytch.com>
2022-04-26 16:19:50 -0700

    Add URL routes to each recipe

--- components/LoginMethodCard.tsx
+++ components/LoginMethodCard.tsx
@@ -2,31 +2,42 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 import { LoginType } from '../lib/types';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 type Props = {
-  login: LoginType;
-  onClick: () => void;
+  recipe: LoginType;
 };
 
-const LoginMethodCard = ({ login, onClick }: Props) => {
+const LoginMethodCard = ({ recipe }: Props) => {
+  const router = useRouter();
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    router.push(`recipes/${recipe.id}`);
+  };
+
   return (
     <div className={styles.card}>
       <div className={styles.cardUpper}>
-        <h2>{login.title}</h2>
-        {login.products && <h3 className={styles.productHeader}>Products:</h3>}
-        {login.products?.map((product) => {
+        <h2>{recipe.title}</h2>
+        {recipe.products && <h3 className={styles.productHeader}>Products:</h3>}
+        {recipe.products?.map((product) => {
           return (
             <div key={product.name} className={styles.productRow}>
               <Image alt="" height={24} width={24} src={product.icon} /> {product.name}
             </div>
           );
         })}
-        <p>{login.details}</p>
-        <p>{login.description}</p>
+        <p>{recipe.details}</p>
+        <p>{recipe.description}</p>
       </div>
       <div className={styles.cardLower}>
-        <button className={styles.entryButton} onClick={onClick}>
-          {login.entryButtonText || `Try now`} 
+        <button
+          disabled={recipe.entryButton?.disabled}
+          className={styles.entryButton}
+          onClick={recipe.entryButton?.onClick || handleClick}
+        >
+          {recipe.entryButton?.text || `Try now`}
         </button>
       </div>
     </div>
---
+++ lib/recipeData.tsx
@@ -0,0 +1,131 @@
+import { LoginType } from './types';
+import LoginWithSMS from '../components/SMSPasscodes/LoginWithSMS';
+import LoginWithEmailWebAuthn from '../components/EmailWebAuthn/LoginWithEmail';
+import LoginWithMagicLinks from '../components/LoginWithReactSDK';
+import LoginProducts from './loginProduct';
+
+export const Recipes: Record<string, LoginType> = {
+  REACT: {
+    id: 'ui-js-sdk',
+    title: 'Pre-built UI + JavaScript SDK',
+    details:
+      'Use our pre-built UI component and JavaScript SDK to get started with Stytch as quickly as possible. The pre-built UI provides a beautiful and customizable login form to make sure your brand stays front and center and the SDK handles everything else for you.',
+    description:
+      'In this recipe we demonstrate a login flow that includes Email magic links and several OAuth options and Google One Tap.',
+    instructions: `To the right you'll see our pre-built login form with several OAuth providers and Email magic links. Below you can see the configuration and customization parameters used to create the login form.`,
+    component: <LoginWithMagicLinks />,
+    products: [LoginProducts.EML, LoginProducts.OAUTH],
+    code: `
+      const sdkStyle: StyleConfig = {
+        fontFamily: '"Helvetica New", Helvetica, sans-serif',
+        primaryColor: '#19303d',
+        primaryTextColor: '#090909',
+      };
+      
+      const magicLinksView = {
+        products: [SDKProductTypes.oauth, SDKProductTypes.emailMagicLinks],
+        emailMagicLinksOptions: {
+          loginRedirectURL: REDIRECT_URL_BASE + '/authenticate?type=em',
+          loginExpirationMinutes: 30,
+          signupRedirectURL: REDIRECT_URL_BASE + '/authenticate?type=em',
+          signupExpirationMinutes: 30,
+          createUserAsPending: false,
+        },
+        oauthOptions: {
+          providers: [
+            { type: OAuthProvidersTypes.Google, one_tap: true, position: 'embedded' },
+            { type: OAuthProvidersTypes.Apple },
+            { type: OAuthProvidersTypes.Microsoft },
+            { type: OAuthProvidersTypes.Facebook },
+            { type: OAuthProvidersTypes.Github },
+          ],
+          loginRedirectURL: REDIRECT_URL_BASE + '/authenticate?type=oauth',
+          signupRedirectURL: REDIRECT_URL_BASE + '/authenticate?type=oauth',
+        },
+      };
+      `,
+  },
+  CUSTOM_UI_HEADLESS: {
+    id: 'headless',
+    title: 'Custom UI + JavaScript SDK',
+    details: `In this example you can see what a typical SMS passcodes (OTP) login flow might look like; here we've built a custom phone number entry and passcode input UI that leverages our JavaScript SDK to handle the heavy lifting.`,
+    description: `This gives you full control over the user experience while minimizing backend code and session logic by using our SDK.`,
+    instructions: `To the right you'll see a phone number entry form built within this example app itself, not using our pre-built UI. Below you can see the two simple SDK calls to send the SMS passcode and authenticate the passcode input by the user.`,
+    component: <LoginWithSMS />,
+    products: [LoginProducts.SMS],
+    code: `    
+  // Send the SMS passcode
+  await stytchClient.otps.sms.loginOrCreate('+1' + phoneNumber);
+  
+  // Verify the passcode input by the user
+  await stytchClient.otps.authenticate(
+    otpInput,
+    methodId,
+    { session_duration_minutes: 30 }
+  );
+      `,
+  },
+  CUSTOM_UI_API: {
+    id: 'api',
+    title: 'Custom UI + API integration',
+    details:
+      'For developers that want full control over the entire experience, you can interact directly with the Stytch API in your backend.',
+    description: `In this example we use custom UI elements and backend API logic to implement a two factor authentication flow with Email magic links as the primary factor and WebAuthn as the secondary factor.`,
+    instructions: `To the right you'll see an email address entry form built within this example app, not using our pre-built UI. You'll start off the flow by using Email magic links as a primary factor, then you'll be prompted to register and authenticate via WebAuthn as a second factor in a multi factor authentication flow.`,
+    component: <LoginWithEmailWebAuthn />,
+    products: [LoginProducts.EML, LoginProducts.WEBAUTHN],
+    code: `
+      // Send the Email magic link
+      await stytchClient.magicLinks.email.loginOrCreate({
+        email: data.email,
+        login_magic_link_url:  REDIRECT_URL_BASE + '/api/authenticate_magic_link_with_webauthn',
+        signup_magic_link_url: REDIRECT_URL_BASE + '/api/authenticate_magic_link_with_webauthn',
+      });
+  
+      // Authenticate the Email magic link
+      await stytchClient.magicLinks.authenticate(token as string);
+      `,
+  },
+  CRYPTO_WALLETS: {
+    id: 'crypto-wallets',
+    title: 'Web3 with Crypto wallets',
+    details:
+      'Our Web3 login products let you seamlessly weave crypto wallets into your traditional Web2 app or your latest Web3 project.',
+    description: `Coming soon.`,
+    instructions: `Coming soon`,
+    component: <LoginWithEmailWebAuthn />,
+    products: [LoginProducts.WEB3],
+    code: ``,
+    entryButton: {
+      text: `Coming soon`,
+      disabled: true,
+    },
+    preventClickthrough: true,
+  },
+  FEEDBACK: {
+    id: 'feedback',
+    title: 'Feedback',
+    details: `Don't see a recipe that you'd like to? Let us know below!`,
+    description: ``,
+    instructions: ``,
+    component: <LoginWithEmailWebAuthn />,
+    code: ``,
+    entryButton: {
+      text: `Send feedback`,
+      onClick: () => {
+        window.open(`mailto:support@stytch.com`);
+      },
+    },
+    preventClickthrough: true,
+  },
+};
+
+export const getRecipeFromId = (id?: string) => {
+  for (const recipe of Object.values(Recipes)) {
+    if (id === recipe.id && !recipe.preventClickthrough) {
+      return recipe;
+    }
+  }
+
+  return null;
+};
