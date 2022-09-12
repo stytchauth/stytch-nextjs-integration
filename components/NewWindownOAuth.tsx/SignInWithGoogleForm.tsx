1b2873f74e1b8f8810d94f72c6a0fde8c86a75a0
Cal Rueb <cal@stytch.com>
2022-09-12 09:37:33 -0700

    URL fixes

--- components/NewWindownOAuth.tsx/SignInWithGoogleForm.tsx
+++ components/NewWindownOAuth.tsx/SignInWithGoogleForm.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { useStytchUser } from '@stytch/nextjs';
-
-import { useRouter } from 'next/router';
+import { getDomainFromWindow } from '../../lib/urlUtils';
 
 declare let window: any;
 
 const SignInWithGoogleForm = () => {
   const { user } = useStytchUser();
-  const router = useRouter();
 
-  const REDIRECT_URL = `http://localhost:3000/oauth-no-redirect`;
+  const REDIRECT_URL = getDomainFromWindow() + `/oauth-no-redirect`;
   const GOOGLE_OAUTH_START_URL = `https://test.stytch.com/v1/public/oauth/google/start?public_token=${process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN}&login_redirect_url=${REDIRECT_URL}&signup_redirect_url=${REDIRECT_URL}`;
   if (user) {
-    return <div>The SDK has detected the user!</div>;
+    return <div>The SDK has detected the user without having to reload the page!</div>;
   }
 
   return (
