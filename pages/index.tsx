a4df9913eb4b51bcb4724f8fe4b5df76cd3b4bac
Cal Rueb <cal@stytch.com>
2022-10-19 14:42:54 -0700

    Margin tweak

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const App = () => {
   }, [user, router]);
 
   return (
-    <div style={{ padding: '0px 40px' }}>
+    <div style={{ padding: '24px 40px' }}>
       <h2>Stytch authentication recipes</h2>
       <p>
         Stytch provides many options to build your perfect passwordless authentication experience including pre-built UI
