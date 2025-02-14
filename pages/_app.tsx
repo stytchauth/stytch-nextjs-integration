b8bae9373a44351e746405669982f0fcfbd403ac
Pei-Yi Lin <167138465+plin-stytch@users.noreply.github.com>
2025-02-13 18:59:45 -0800

    [WEB-3610] Rebrand (#168)
    * Update favicon, logo, bg color
    
    * Update bg
    
    * Update Chivo too

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,11 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <title>Stytch Demo</title>
       </Head>
-
       <StytchProvider stytch={stytch}>
         <NavBar />
         <Component {...pageProps} />
